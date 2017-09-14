8b46632f61306bce8f1b69ed0be021444ab0077b
huangjianwu <nova_huangjw@126.com>
2017-09-14 14:24:35 +0800

    support swift3

--- Pod/Classes/IntervalSlider.swift
+++ Pod/Classes/IntervalSlider.swift
@@ -13,9 +13,9 @@ public protocol IntervalSliderDelegate {
 }
 
 public class IntervalSliderSource {
-  private var validValue: Float
-  private var appearanceValue: Float
-  private var label: UILabel?
+  fileprivate var validValue: Float
+  fileprivate var appearanceValue: Float
+  fileprivate var label: UILabel?
   
   /**
   Initialize IntervalSliderSource
@@ -52,39 +52,39 @@ public class IntervalSlider: UIView {
       fatalError("init(coder:) has not been implemented")
     }
 
-    override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
       return true
     }
   }
   
   public var delegate: IntervalSliderDelegate?
-  private var slider: TapSlider!
-  private var sources = [IntervalSliderSource]()
-  private var labels = [UILabel]()
-  private var marks = [UIView]()
+  fileprivate var slider: TapSlider!
+fileprivate var sources = [IntervalSliderSource]()
+  fileprivate var labels = [UILabel]()
+  fileprivate var marks = [UIView]()
   
   // options
-  private var minimumValue: Float = 0 {
+  fileprivate var minimumValue: Float = 0 {
     willSet {
       if newValue < 0 || newValue > 100 { fatalError("MinimumValue must be between 0 and 100") }
     }
   }
-  private var maximumValue: Float = 100 {
+  fileprivate var maximumValue: Float = 100 {
     willSet {
       if newValue < 0 || newValue > 100 { fatalError("MaximumValue must be between 0 and 100") }
     }
   }
-  private var minimumTrackTintColor = UIColor.blueColor()
-  private var labelBottomPadding: CGFloat = 5
-  private var defaultValue: Float = 0
-  private var isAddMark: Bool = false
-  private var thumbImage: UIImage?
-  private var defaultThumbImage: UIImage {
-    let thumbView = UIView(frame: CGRectMake(0, 0 , 20, 20))
-    thumbView.backgroundColor = UIColor.whiteColor()
+  fileprivate var minimumTrackTintColor = UIColor.blue
+  fileprivate var labelBottomPadding: CGFloat = 5
+  fileprivate var defaultValue: Float = 0
+  fileprivate var isAddMark: Bool = false
+  fileprivate var thumbImage: UIImage?
+  fileprivate var defaultThumbImage: UIImage {
+    let thumbView = UIView(frame:CGRect(x: 0, y: 0, width: 20, height: 20))
+    thumbView.backgroundColor = UIColor.white
     thumbView.layer.cornerRadius = thumbView.bounds.width * 0.5
-    thumbView.clipsToBounds = true    
-    return self.imageFromViewWithCornerRadius(thumbView)
+    thumbView.clipsToBounds = true
+    return self.imageFromViewWithCornerRadius(view: thumbView)
   }
   
   required public init?(coder aDecoder: NSCoder) {
@@ -94,8 +94,8 @@ public class IntervalSlider: UIView {
   public init(frame: CGRect, sources: [IntervalSliderSource], options: [IntervalSliderOption]? = nil) {
     super.init(frame: frame)
     self.sources = sources
-    self.build(options)
-    self.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+    self.build(options: options)
+    self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
   }
   
   override public func layoutSubviews() {
@@ -126,53 +126,53 @@ public class IntervalSlider: UIView {
     }
     let rect = CGRect(x: 0, y:self.frame.height / 2, width: self.frame.width, height: self.frame.height / 2)
     self.slider = TapSlider(frame: rect)
-    self.slider.setThumbImage(self.thumbImage ?? self.defaultThumbImage, forState: .Normal)
+    self.slider.setThumbImage(self.thumbImage ?? self.defaultThumbImage, for: .normal)
     self.addSubview(self.slider)
     self.slider.minimumValue = self.minimumValue
     self.slider.maximumValue = self.maximumValue
     self.slider.minimumTrackTintColor = self.minimumTrackTintColor
-    self.slider.addTarget(self, action: Selector("endDrag:"), forControlEvents: .TouchUpInside)
-    self.slider.addTarget(self, action: Selector("endDrag:"), forControlEvents: .TouchUpOutside)
+//    self.slider.addTarget(self, action: Selector(("endDrag:")), for: .touchUpInside)
+//    self.slider.addTarget(self, action: Selector(("endDrag:")), for: .touchUpOutside)
+    self.slider.addTarget(self, action: #selector(endDrag(slider:)), for: .touchUpInside)
+    self.slider.addTarget(self, action: #selector(endDrag(slider:)), for: .touchUpOutside)
 
     for source in self.sources {
       if let label = source.label {
-        label.frame = CGRectMake(0, 0, label.bounds.size.width, label.bounds.size.height)
+        label.frame = CGRect(x: 0, y: 0, width: label.bounds.size.width, height: label.bounds.size.height)
         self.labels.append(label)
         self.addSubview(label)
       }
       if self.isAddMark {
-        let mark = UIView(frame: CGRectMake(0, 0, 1, 8))
-        mark.backgroundColor = UIColor.lightGrayColor()
+        let mark = UIView(frame:CGRect(x: 0, y: 0, width: 1, height: 8))
+        mark.backgroundColor = UIColor.lightGray
         self.marks.append(mark)
         self.insertSubview(mark, belowSubview:self.slider)
       }
     }
-    self.seek(self.adjustSliderValue(self.defaultValue))
+    self.seek(value: self.adjustSliderValue(baseValue: self.defaultValue))
   }
   
   private func layout(){
-    self.slider.frame = CGRectMake(0, self.frame.height / 2, self.frame.width, self.frame.height / 2)
+    self.slider.frame = CGRect(x: 0, y: self.frame.height / 2, width: self.frame.width, height: self.frame.height)
     var index: Int = 0
     for source in self.sources {
       let label = self.labels[index]
-      let center = self.thumbCenter(source.appearanceValue)
-      label.frame = CGRectMake(0,
-        self.slider.frame.origin.y - (label.bounds.size.height + self.labelBottomPadding),
-        label.bounds.size.width,
-        label.bounds.size.height)
+      let center = self.thumbCenter(value: source.appearanceValue)
+       
+      label.frame =  CGRect(x: 0, y: self.slider.frame.origin.y - (label.bounds.size.height + self.labelBottomPadding), width: label.bounds.size.width, height: label.bounds.size.height)
       label.center = CGPoint(x: center.x, y: label.center.y)
       if self.isAddMark {
         let mark = marks[index]
         mark.center = CGPoint(x: center.x, y: center.y)
       }
-      index++
+      index += 1
     }
   }
   
   public func endDrag(slider: UISlider) {
-    let adjustValue: Float = self.adjustSliderValue(self.slider.value)
-    self.seek(adjustValue)
-    self.delegate?.confirmValue(self, validValue: self.getValue())
+    let adjustValue: Float = self.adjustSliderValue(baseValue: self.slider.value)
+    self.seek(value: adjustValue)
+    self.delegate?.confirmValue(slider: self, validValue: self.getValue())
   }
   
   /**
@@ -180,7 +180,7 @@ public class IntervalSlider: UIView {
   
   */
   public func getValue() -> Float {
-    return self.findValidValue(self.slider.value)
+    return self.findValidValue(appearanceValue: self.slider.value)
   }
   
   /**
@@ -191,7 +191,7 @@ public class IntervalSlider: UIView {
   - parameter value:   validValue
   */
   public func setValue(value: Float) {
-    self.seek(self.findAppearanceValue(value))
+    self.seek(value: self.findAppearanceValue(validValue: value))
   }
   
   private func findValidValue(appearanceValue: Float) -> Float {
@@ -210,10 +210,10 @@ public class IntervalSlider: UIView {
 
   private func thumbCenter(value: Float) -> (x: CGFloat, y: CGFloat) {
     let originValue = self.slider.value
-    self.seek(value)
-    let trackRect = self.slider.trackRectForBounds(self.slider.bounds)
-    let thumbRect = self.slider.thumbRectForBounds(self.slider.bounds, trackRect: trackRect, value: self.slider.value)
-    self.seek(originValue)
+    self.seek(value: value)
+    let trackRect = self.slider.trackRect(forBounds: self.slider.bounds)
+    let thumbRect = self.slider.thumbRect(forBounds: self.slider.bounds, trackRect: trackRect, value: self.slider.value)
+    self.seek(value: originValue)
     return (thumbRect.origin.x + (self.slider.currentThumbImage!.size.width / 2),
       self.slider.frame.origin.y + thumbRect.origin.y + (self.slider.currentThumbImage!.size.height / 2))
   }
@@ -237,10 +237,10 @@ public class IntervalSlider: UIView {
   }
   
   private func seek(value: Float) {
-    UIView.animateWithDuration(0.6, delay: 0,
+    UIView.animate(withDuration: 0.6, delay: 0,
       usingSpringWithDamping: 0.4,
       initialSpringVelocity: 0,
-      options: .CurveEaseInOut,
+      options: .curveEaseInOut,
       animations: {
         self.slider.setValue(value, animated: true)
       },
@@ -249,7 +249,8 @@ public class IntervalSlider: UIView {
   
   private func imageFromViewWithCornerRadius(view: UIView) -> UIImage {
     // maskImage
-    let imageBounds = CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height)
+    
+    let imageBounds = CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height)
     let path = UIBezierPath(roundedRect: imageBounds, cornerRadius: view.bounds.size.width * 0.5)
     UIGraphicsBeginImageContextWithOptions(path.bounds.size, false, 0)
     view.backgroundColor?.setFill()
@@ -260,10 +261,10 @@ public class IntervalSlider: UIView {
     // drawImge
     UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
     let context = UIGraphicsGetCurrentContext()
-    CGContextClipToMask(context, imageBounds, maskImage.CGImage)
-    view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+    context!.clip(to: imageBounds, mask: (maskImage?.cgImage)!)
+    view.layer.render(in: UIGraphicsGetCurrentContext()!)
     let image = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
-    return image;
+    return image!;
   }
 }
